9c4e5d5dc48f95f515a1b0b14656eecae5d4caef
berk <berk@berks-imac.local>
2017-04-20 20:46:01 -0400

    Initial Commit

---
+++ MemoryPuzzle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemoryPuzzle
+//
+//  Created by berk on 4/20/17.
+//  Copyright © 2017 berk_kaan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MemoryPuzzle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MemoryPuzzle
+//
+//  Created by berk on 4/20/17.
+//  Copyright © 2017 berk_kaan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
